b540f257328566c20461a1af5cd72e5fd093cab4
sgupta1703 <sarthakgupta1703@gmail.com>
2025-09-18 12:31:17 -0400

    up

--- FrontEnd/src/Components/supabaseClient.js
+++ FrontEnd/src/Components/supabaseClient.js
@@ -1,7 +1,6 @@
-
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://trhogwinqistpnnpidql.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyaG9nd2lucWlzdHBubnBpZHFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2MDc1NjIsImV4cCI6MjA3MzE4MzU2Mn0.yB8Z4td_-xMCwEuAyupgK3Ji877CpC6atxKF77G-r3s";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
