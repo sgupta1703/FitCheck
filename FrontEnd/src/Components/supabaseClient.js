304804e88760c632a8bfb428a728b85dc9a7fdc7
sgupta1703 <sarthakgupta1703@gmail.com>
2025-09-18 12:25:03 -0400

    updated supabaseClient.js to use environment variables for security

--- FrontEnd/src/Components/supabaseClient.js
+++ FrontEnd/src/Components/supabaseClient.js
@@ -1,6 +1,7 @@
+
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://trhogwinqistpnnpidql.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyaG9nd2lucWlzdHBubnBpZHFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2MDc1NjIsImV4cCI6MjA3MzE4MzU2Mn0.yB8Z4td_-xMCwEuAyupgK3Ji877CpC6atxKF77G-r3s";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
